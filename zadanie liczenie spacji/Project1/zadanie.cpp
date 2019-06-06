b237c2a0463367f850ef2e4f093e7c0c63ee95ce
KarolKluka <karolkluka7@gmail.com>
2019-06-06 10:28:05 +0200

    liczenie spacji

---
+++ zadanie liczenie spacji/Project1/zadanie.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+void tekst()
+{
+	int znaki;
+	cin >> znaki;
+
+}
+
+void wypisz()
+{
+	int liczba_sp;
+	if (znaki[i] == ' ')
+		liczba_sp++;
+	cout << liczba_sp;
+}
